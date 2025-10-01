935297fc9b206ff11db30f867699e2039a3f972d
Augustin Riedinger <augustin.riedinger@gmail.com>
2025-10-01 18:12:49 +0200

    codebase upgrade

--- app/js/pages/HelpPage.js
+++ app/js/pages/HelpPage.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import { Link } from 'react-router-dom';
+import { marked } from 'marked';
 
 import mdContent from '../content/help.md'
 
 const ConfigPage = () => {
+  const htmlContent = marked(mdContent);
+  
   return (
     <Layout mode="container">
-      <div className="content" dangerouslySetInnerHTML={{__html: mdContent}} />
+      <div className="content" dangerouslySetInnerHTML={{__html: htmlContent}} />
       <div className="has-text-centered">
         <Link className="button is-primary" to="/">Revenir à l'écran d'accueil</Link>
       </div>
